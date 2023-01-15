c0db2627ba924739fdc21968b4e58ea5a4c72ae5
Zheng Can <zc3593835@gmail.com>
2023-01-15 14:11:06 +0800

    up

--- rust/build.sh
+++ rust/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-docker build --target builder --tag zhengcan/rust:builder-aarch64 -f Dockerfile.aarch64 .
+docker build --target builder --tag zhengcan/rust-aarch64:builder -f Dockerfile.aarch64 .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 
-docker build --target runtime --tag zhengcan/rust:runtime-aarch64 -f Dockerfile.aarch64 .
+docker build --target runtime --tag zhengcan/rust-aarch64:runtime -f Dockerfile.aarch64 .
+r=$? && if [ $r != 0 ]; then exit $r; fi
+
+docker build --target runtime-opencv --tag zhengcan/rust-aarch64:runtime-opencv -f Dockerfile.aarch64 .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 
 docker build -f Dockerfile.aarch64 .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 
-docker push zhengcan/rust:builder-aarch64
-docker push zhengcan/rust:runtime-aarch64
+docker push zhengcan/rust-aarch64:builder
+docker push zhengcan/rust-aarch64:runtime
+docker push zhengcan/rust-aarch64:runtime-opencv
 
 
