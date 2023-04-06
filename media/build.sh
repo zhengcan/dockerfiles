eee755d8214d3ad0aae322219137498dd230fed5
canzheng <canzheng@tencent.com>
2023-04-06 13:09:52 +0800

    support ffmpeg & opencv

---
+++ media/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker build \
+  --build-arg BUILD_IMG=tencentos/tencentos_server31 \
+  --build-arg RUNTIME_IMG=tencentos/tencentos_server31 \
+  --tag zhengcan/ffmpeg-opencv-runtine .
+r=$? && if [ $r != 0 ]; then exit $r; fi
+
+docker push zhengcan/ffmpeg-opencv-runtine
+
+
