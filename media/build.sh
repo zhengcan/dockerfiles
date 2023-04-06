c82a4ddb300cadcd300bbd6e5d035ee29ec831ff
canzheng <canzheng@tencent.com>
2023-04-06 13:32:30 +0800

    add install-deps

--- media/build.sh
+++ media/build.sh
@@ -3,9 +3,9 @@
 docker build \
   --build-arg BUILD_IMG=tencentos/tencentos_server31 \
   --build-arg RUNTIME_IMG=tencentos/tencentos_server31 \
-  --tag zhengcan/ffmpeg-opencv-runtine .
+  --tag zhengcan/ffmpeg-opencv-runtime .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 
-docker push zhengcan/ffmpeg-opencv-runtine
+docker push zhengcan/ffmpeg-opencv-runtime
 
 
