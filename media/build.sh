d9dc73e4b9b7cb6378fc4ba3bbd1a46f997115df
canzheng <canzheng@tencent.com>
2023-07-24 16:11:25 +0800

    try opencv 4.8.0

--- media/build.sh
+++ media/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+SUFFIX=
+BUILDER_IMG=zhengcan/ffmpeg-opencv-runtime:bd${SUFFIX}
+STANDARD_IMG=zhengcan/ffmpeg-opencv-runtime:latest${SUFFIX}
+TACC_IMG=mirrors.tencent.com/tacc/ffmpeg-opencv-runtime:latest${SUFFIX}
+RUST_IMG=zhengcan/ffmpeg-opencv-rust:latest${SUFFIX}
+
 # Builder
 docker build \
-  --tag zhengcan/ffmpeg-opencv-runtime:bd \
+  --tag ${BUILDER_IMG} \
   -f Dockerfile.bd \
   .
 r=$? && if [ $r != 0 ]; then exit $r; fi
@@ -10,14 +16,16 @@ r=$? && if [ $r != 0 ]; then exit $r; fi
 # Runtime
 # - standard
 docker build \
-  --tag zhengcan/ffmpeg-opencv-runtime \
+  --build-arg BUILDER_IMG=${BUILDER_IMG} \
+  --tag ${STANDARD_IMG} \
   --target standard \
   -f Dockerfile.rt \
   .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 # - tacc
 docker build \
-  --tag mirrors.tencent.com/tacc/ffmpeg-opencv-runtime \
+  --build-arg BUILDER_IMG=${BUILDER_IMG} \
+  --tag ${TACC_IMG} \
   --target tacc \
   -f Dockerfile.rt \
   .
@@ -25,15 +33,15 @@ r=$? && if [ $r != 0 ]; then exit $r; fi
 
 # with Rust
 docker build \
-  --tag zhengcan/ffmpeg-opencv-rust \
+  --tag ${RUST_IMG} \
   -f Dockerfile.rs \
   .
 r=$? && if [ $r != 0 ]; then exit $r; fi
 
 # Push
-if [[ "$PUSH" != "n" ]] | [[ "$PUSH" != "N" ]]; then
-  docker push zhengcan/ffmpeg-opencv-runtime
-  docker push mirrors.tencent.com/tacc/ffmpeg-opencv-runtime
-  docker push zhengcan/ffmpeg-opencv-rust
+if [[ "$PUSH" != "n" ]] && [[ "$PUSH" != "N" ]]; then
+  docker push ${STANDARD_IMG}
+  docker push ${TACC_IMG}
+  docker push ${RUST_IMG}
 fi
 
