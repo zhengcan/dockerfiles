b8185f1dd704030d63774aa064c870f9ec37d441
Zheng Can <zc3593835@gmail.com>
2024-05-09 14:09:38 +0800

    update

--- all-in-one/build.sh
+++ all-in-one/build.sh
@@ -8,10 +8,5 @@ docker build -t zhengcan/run:media                              --target media  
 docker build -t zhengcan/run:jdk-17 --build-arg JDK_TAG=17-jdk  --target java         -f Dockerfile.runtime .
 docker build -t zhengcan/run:jre-17 --build-arg JDK_TAG=17-jre  --target java         -f Dockerfile.runtime .
 
-docker push zhengcan/dev:media-bd
-docker push zhengcan/dev:media
-docker push zhengcan/dev:builder
-docker push zhengcan/run:simple
-docker push zhengcan/run:media
-docker push zhengcan/run:jdk-17
-docker push zhengcan/run:jre-17
+docker push -a zhengcan/dev
+docker push -a zhengcan/run
