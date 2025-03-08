df37ef20fc3129c8f3044811721ba05677ba35af
Zheng Can <zc3593835@gmail.com>
2025-03-08 15:52:03 +0000

    update

--- all-in-one/build.sh
+++ all-in-one/build.sh
@@ -21,3 +21,17 @@ if [[ "$DRY_RUN" == "" ]]; then
   docker push -a zhengcan/dev
   docker push -a zhengcan/run
 fi
+
+if [[ "$OKE" != "" ]]; then
+  docker tag zhengcan/dev:media    mel.ocir.io/ax1j1s8oewtm/zhengcan/dev:media
+  docker tag zhengcan/dev:media-bd mel.ocir.io/ax1j1s8oewtm/zhengcan/dev:media-bd
+  docker tag zhengcan/dev:malloc   mel.ocir.io/ax1j1s8oewtm/zhengcan/dev:malloc
+  docker tag zhengcan/dev:builder  mel.ocir.io/ax1j1s8oewtm/zhengcan/dev:builder
+  docker tag zhengcan/run:simple   mel.ocir.io/ax1j1s8oewtm/zhengcan/run:simple
+  docker tag zhengcan/run:media    mel.ocir.io/ax1j1s8oewtm/zhengcan/run:media
+  docker tag zhengcan/run:jdk-17   mel.ocir.io/ax1j1s8oewtm/zhengcan/run:jdk-17
+  docker tag zhengcan/run:jre-17   mel.ocir.io/ax1j1s8oewtm/zhengcan/run:jre-17
+  docker push -a mel.ocir.io/ax1j1s8oewtm/zhengcan/dev
+  docker push -a mel.ocir.io/ax1j1s8oewtm/zhengcan/run
+fi
+
